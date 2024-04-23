e27d326d841bca7d80961fdf0283371d283ee5fa
Bruce-Hopkins <behopkinsjr@gmail.com>
2024-04-23 22:14:21 +0200

    inprogress: adding undo struct

--- core-lib/src/undo.rs
+++ core-lib/src/undo.rs
@@ -1,50 +1,88 @@
 use std::collections::VecDeque;
 
-use crate::position::Position;
+use crate::position::{self, Position};
 use crate::selection::Range;
 
-struct Undo {
-    undo_stack: TextStack,
-    redo_stack: TextStack
+pub struct Undo {
+    undo_stack: UndoStack,
+    redo_stack: UndoStack,
+    current_item: Option<TextChange>
 }
 
-struct TextStack(VecDeque<TextChange>);
 
 impl Undo {
-    pub fn add() {
+
+    pub fn new(capacity: usize) -> Self {
+        Undo {
+            undo_stack: UndoStack::new(capacity),
+            redo_stack: UndoStack::new(capacity),
+            current_item: None
+        }
+    }
+
+    pub fn add(&mut self, text:String, position: Position) {
+        self.undo_stack.push(TextChange::Insert(position, text));
+    }
+    
+    fn replace_current_item(&mut self, change:TextChange) {
+        if let Some(current_item) = self.current_item.as_mut() {
+
+            if let (TextChange::Insert(current_item_pos, current_item_text), TextChange::Insert(position, change_text)) = (current_item, &change) {
+                current_item_text.push_str(change_text);
+                let pos= 
+                return
+            }
+            else if let (TextChange::Delete(_, _), TextChange::Delete(_, _)) = (current_item, &change) {
+                
+            }
+            else if let (TextChange::Replace(_, _), TextChange::Replace(_, _)) = (current_item, &change) {
+                
+            }            
+        }
+        self.current_item = Some(change)
+    }
+
+    pub fn delete(&mut self, text:String, range: Range) {
+        self.undo_stack.push(TextChange::Delete(range, text))
+    }
+
+    pub fn replace(&mut self, text:String, range: Range) {
+        self.undo_stack.push(TextChange::Replace(range, text))
+    }
+
+    pub fn push(&mut self) {
         todo!()
     }
 
-    pub fn push() {
+    pub fn redo(&mut self) -> TextChange {
         todo!()
     }
 
-    pub fn redo() {
-        todo!()
-    }
-
-    pub fn undo() {
+    pub fn undo(&mut self) -> TextChange {
         todo!()
     }
 }
 
-enum TextChange {
-    Insert {
-        position: Position,
-        text: String
-    },
-    Delete {
-        position: Range,
-        text: String,
-    },
-    Replace {
-        position: Range,
-        text: String
+struct UndoStack(VecDeque<TextChange>);
+impl  UndoStack{
+
+    fn new(capacity:usize) -> Self {
+        Self(VecDeque::with_capacity(capacity))
     }
+
+    fn push(&mut self, change:TextChange) {
+        todo!()
+    }
+}
+
+pub enum TextChange {
+    Insert(Position,String),
+    Delete(Range,String),
+    Replace(Range,String)
 }
 
 impl TextChange {
-    fn reverse(&self) {
+    fn reverse(&mut self) {
         todo!()
     }
 }
--- core-lib/tests/undo.rs
+++ core-lib/tests/undo.rs
@@ -1,9 +1,96 @@
 #[cfg(test)]
 mod undo_tests {
 
+    use core_lib::{undo::{Undo, TextChange}, position::Position, selection::Range};
+
+
     #[test]
     fn should_undo() {
+        let mut undo = Undo::new(10);
+        undo.add(String::from("123"), Position::new(0, 0));
+        undo.push();
+    
+        undo.add(String::from("456"), Position::new(0, 3));
+        undo.push();
 
+        if let TextChange::Delete {range, text } = undo.undo() {
+            assert_eq!(text, String::from("456"));
+            assert_eq!(range, Range::new(Position::new(0, 2), Position::new(0, 5)));
+        }
+        else {
+            panic!("Incorrect text change type");
+        }
+    }
+
+    #[test]
+    fn should_redo() {
+
+        let mut undo = Undo::new(10);
+        undo.add(String::from("123"), Position::new(0, 0));
+        undo.push();
+    
+        undo.add(String::from("456"), Position::new(0, 3));
+        undo.push();
+
+        undo.undo();
+        
+        if let TextChange::Insert { position, text } = undo.redo() {
+            assert_eq!(text, String::from("456"));
+            assert_eq!(position, Position::new(0, 3));
+        }
+        else {
+            panic!("Incorrect text change type");
+        }
+        
+    }
+
+    #[test]
+    fn should_undo_delete() {
+
+        let mut undo = Undo::new(10);
+        undo.add(String::from("123"), Position::new(0, 0));
+        undo.push();
+    
+        undo.delete(String::from("123"), Range::new(Position::new(0, 0), Position::new(0,2)));
+        undo.push();
+
+        undo.undo();
+
+        if let TextChange::Insert { position, text } = undo.undo() {
+            assert_eq!(text, String::from("456"));
+            assert_eq!(position, Position::new(0, 3));
+        }
+        else {
+            panic!("Incorrect text change type");
+        }
+    }
+
+    #[test]
+    fn should_undo_replace() {
+
+        let mut undo = Undo::new(10);
+        undo.add(String::from("123"), Position::new(0, 0));
+        undo.push();
+    
+        undo.replace(String::from("456"), Range::new(Position::new(0, 0), Position::new(0,2)));
+        undo.push();
+
+        undo.undo();
+
+        if let TextChange::Replace{range, text } = undo.undo() {
+            assert_eq!(text, String::from("123"));
+            assert_eq!(range, Range::new(Position::new(0, 0), Position::new(0,2)));
+        }
+        else {
+            panic!("Incorrect text change type");
+        }
+        if let TextChange::Replace{range, text } = undo.redo() {
+            assert_eq!(text, String::from("456"));
+            assert_eq!(range, Range::new(Position::new(0, 0), Position::new(0,2)));
+        }
+        else {
+            panic!("Incorrect text change type");
+        }
     }
 
 }
